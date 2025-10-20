b52fc7cee404ac9de04167fc4318bf4a97d4d632
Ahryxx <phvanthanh06@gmail.com>
2025-10-20 23:11:52 +0700

    chore(eslint): add flat config for ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**", "data/**", "dist/**"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        module: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "semi": ["error", "always"],
+      "quotes": ["error", "single", { "avoidEscape": true }]
+    }
+  }
+];
